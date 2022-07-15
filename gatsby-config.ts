564837453496b60aae627e8524e9dd10395fa8bd
Krzysztof Orkisz <krzysztof.orkisz@softwareone.com>
2022-07-15 22:14:11 +0200

    add tailwind

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,6 +11,7 @@ const config: GatsbyConfig = {
   graphqlTypegen: true,
   plugins: [
     `gatsby-plugin-layout`,
+    'gatsby-plugin-postcss',
     'gatsby-plugin-sass',
     // {
     //   resolve: 'gatsby-plugin-google-analytics',
