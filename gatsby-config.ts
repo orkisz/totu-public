c7f2b17759843e6196716125db2095ac6e27b0e9
Krzysztof Orkisz <krzysztof.orkisz@softwareone.com>
2022-07-18 09:54:57 +0200

    use prefix paths

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,7 @@
 import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
+  pathPrefix: `/totu-public`,
   siteMetadata: {
     title: `TOTU`,
     siteUrl: `https://www.yourdomain.tld`
